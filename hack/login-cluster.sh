74d229a764d7347537334d8fba6c1b2207c6bc54
Adrian Edwards <adredwar@redhat.com>
2025-09-10 10:09:53 -0400

    update login for OSPO cluster

--- hack/login-cluster.sh
+++ hack/login-cluster.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 # oc login --web --server=https://api.cairo.test.nerc.mghpcc.org:6443
-oc login --web --server=https://api.platform-sts.pcbk.p1.openshiftapps.com:6443
+# oc login --web --server=https://api.platform-sts.pcbk.p1.openshiftapps.com:6443
+oc login --web --server=https://api.ospo-osci.z3b1.p1.openshiftapps.com:6443
